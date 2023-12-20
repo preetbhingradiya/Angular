50aba336c407961408c4f6564409c6f23e267b74
preetbhingradiya <preetbhingradiya6@gmail.com>
2023-12-20 19:54:51 +0530

    if-else

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -1,17 +1,20 @@
 import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-notification',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterOutlet],
   templateUrl: './notification.component.html',
   styleUrl: './notification.component.scss'
 })
 export class NotificationComponent {
     count:number=5
     image:string="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.1546980028.1703030400&semt=sph"
-
-    name:string="preet"
+    show:boolean=false
+    name:string=""
     qty=signal<number>(0);  //signal propaty use in tempete with ()
 
     OnChangeName(changeName:any){
