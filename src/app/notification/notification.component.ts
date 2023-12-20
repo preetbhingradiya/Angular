182d436ae7e4fea7787ee34b4fa696fe6634bf28
preetbhingradiya <preetbhingradiya6@gmail.com>
2023-12-20 12:38:51 +0530

    property-binding

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -9,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class NotificationComponent {
     count:number=5
+    image:string="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.1546980028.1703030400&semt=sph"
 }
