e3e3f23859ed35e9547a9e36e6cc38c1e18acce8
preetbhingradiya <preetbhingradiya6@gmail.com>
2023-12-21 15:50:17 +0530

    style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NotificationComponent } from './notification/notification.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,NotificationComponent],
+  imports: [CommonModule, RouterOutlet,NotificationComponent,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
