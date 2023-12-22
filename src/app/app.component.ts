63794fa747e2664f6fad5a05f8d6877ac153a65c
preetbhingradiya <preetbhingradiya6@gmail.com>
2023-12-22 12:27:11 +0530

    todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NotificationComponent } from './notification/notification.component';
 import { HeaderComponent } from './header/header.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,NotificationComponent,HeaderComponent],
+  imports: [CommonModule, RouterOutlet,NotificationComponent,HeaderComponent,TodoListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button'
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,MatButtonModule],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.scss'
 })
 export class TodoListComponent {
-
+  texts:any[]=[];
+  getData(text:any){
+    this.texts.push({id:this.texts.length,name:text})
+  }
+  removeData(id:number){
+    this.texts=this.texts.filter(item=>item.id!==id)
+  }
 }
