1aa4dcc90f0d1a21c060ab4abc3e6b2156b01541
preetbhingradiya <preetbhingradiya6@gmail.com>
2023-12-31 14:54:46 +0530

    decorater

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NotificationComponent } from './notification/notification.component';
 import { HeaderComponent } from './header/header.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { TestComponent } from './test/test.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,NotificationComponent,HeaderComponent,TodoListComponent],
+  imports: [CommonModule, RouterOutlet,NotificationComponent,HeaderComponent,TodoListComponent,TestComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular';
+  fullName:string=''
+  @ViewChildren('inputEl') inputBox:QueryList<ElementRef> 
+
+  show(){
+    let name=''
+    this.inputBox.forEach((ele)=>{
+      name += ele.nativeElement.value + ' '
+    })
+    this.fullName=name
+  }
 }
---
+++ src/app/test/test.component.ts
@@ -0,0 +1,22 @@
+import { Component, ContentChild, ContentChildren, ElementRef, QueryList } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  standalone: true,
+  imports: [],
+  templateUrl: './test.component.html',
+  styleUrl: './test.component.scss'
+})
+export class TestComponent {
+  @ContentChild('para') paragrahEl:ElementRef
+  //use to contentChilde access to the ng-content tag refrence value (ONLY ON TAG)
+  
+
+  @ContentChildren('para') paragrah:QueryList<ElementRef>
+  //use to contentChildren access to the ng-content tag refrence value (MULTIPAL ON TAG)
+
+  childe(){
+    // console.log(this.paragrahEl.nativeElement)
+    this.paragrah.forEach((ele)=>console.log(ele.nativeElement))
+  }
+}
