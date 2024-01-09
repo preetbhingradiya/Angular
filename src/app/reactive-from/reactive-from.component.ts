29abc9a1979eaf5449fb63b217adb0f7b15fdb1e
preetbhingradiya <preetbhingradiya6@gmail.com>
2024-01-09 11:30:10 +0530

    concat

--- src/app/reactive-from/reactive-from.component.ts
+++ src/app/reactive-from/reactive-from.component.ts
@@ -1,28 +1,54 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import {  FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-from',
   standalone: true,
-  imports: [ReactiveFormsModule,CommonModule],
+  imports: [ReactiveFormsModule, CommonModule,FormsModule],
   templateUrl: './reactive-from.component.html',
 })
 export class ReactiveFromComponent {
-  loginForm=new FormGroup({
-    user:new FormControl('',[Validators.required,Validators.pattern('[a-zA-Z]+$')]),
-    password:new FormControl('',[Validators.required,Validators.minLength(5)])
+  username:string='';
+
+  loginForm = new FormGroup({
+    firstname: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z]+$')]),
+    lastname: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z]+$')]),
+    bod: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required])
   })
 
-  loginUser(){
-    console.log(this.loginForm.value)
+  loginUser() {
+
+
+    if (this.loginForm.value.firstname.length >= 3) {
+      this.username += this.loginForm.value.firstname.slice(0, 3)
+    } else {
+      this.username += this.loginForm.value.firstname
+    }
+
+    if (this.loginForm.value.lastname.length >= 3) {
+      this.username += this.loginForm.value.lastname.slice(0, 3)
+    } else {
+      this.username += this.loginForm.value.lastname
+    }
+
+    let bodYear = new Date(this.loginForm.value.bod)
+    this.username += bodYear.getFullYear()
+    this.username = this.username.toLocaleLowerCase()
+
+    console.log(this.username)
   }
 
-  get userValidation(){
-    return this.loginForm.get('user')
+  get fnameValidation() {
+    return this.loginForm.get('firstname')
   }
 
-  get passwordValidation(){
+  get lnameValidation() {
+    return this.loginForm.get('lastname')
+  }
+
+  get passwordValidation() {
     return this.loginForm.get('password')
   }
 }
